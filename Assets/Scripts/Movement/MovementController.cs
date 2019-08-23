bc13cbc57023c50f3fd50f5d919d3a27fd59c950
Cao Gaoting <caogtaa@gmail.com>
2019-08-23 15:50:18 +0800

    to #1, simplify moving logic

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -15,6 +15,11 @@ public class MovementController : MonoBehaviour
     public GameObject lastCollidedObject;
     public DIRECTION_BUTTON lastCollisionDir = DIRECTION_BUTTON.NONE;
 
+    public Vector3 destPosition;
+    public DIRECTION_BUTTON lastMoveDir = DIRECTION_BUTTON.DOWN;    // hardcode init state
+    public bool mIsMoving = false;
+    private int changeDirCoolDown = 0;
+
     void Start()
     {
         if (!animator)
@@ -29,15 +34,17 @@ public class MovementController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!CanMove() && IsMoving())
-        {
-            StopMoving();
-        }
-
         DIRECTION_BUTTON dir = InputController.GetPressedDirectionButton();
         ACTION_BUTTON action = InputController.GetPressedActionButton();
 
-        TriggerButtons(dir, action);
+        if (CanMoveManually()) {
+            Move2(dir);
+        } else {
+            // need auto move in some case
+            Move2();
+        }
+
+        Interact(lastMoveDir, action);
     }
 
     public DIRECTION_BUTTON GetFaceDirection()
@@ -82,6 +89,23 @@ public class MovementController : MonoBehaviour
         }
     }
 
+    public Vector3 GetMovementVector(DIRECTION_BUTTON dir)
+    {
+        switch (dir)
+        {
+            case DIRECTION_BUTTON.UP:
+                return Vector3.up;
+            case DIRECTION_BUTTON.DOWN:
+                return Vector3.down;
+            case DIRECTION_BUTTON.LEFT:
+                return Vector3.left;
+            case DIRECTION_BUTTON.RIGHT:
+                return Vector3.right;
+            default:
+                return Vector3.zero;
+        }
+    }
+
     public bool CanMove()
     {
         // TODO optimize with events
@@ -90,7 +114,7 @@ public class MovementController : MonoBehaviour
 
     public bool CanMoveManually()
     {
-        return CanMove();
+        return !FindObjectOfType<DialogManager>().InDialog();
     }
 
     private void MovementUpdate(DIRECTION_BUTTON dir)
@@ -184,6 +208,90 @@ public class MovementController : MonoBehaviour
         return MoveTo(dir, destinationPosition);
     }
 
+
+    private void Move2(DIRECTION_BUTTON dir = DIRECTION_BUTTON.NONE) {
+        if (IsMoving2()) {
+            // continue moving to destination, can not change direction in the middle
+            float delta = Time.fixedDeltaTime * speed;
+            transform.position = Vector3.MoveTowards(transform.position, destPosition, delta);
+            if (transform.position == destPosition) {
+                StopMoving2();
+            } else if (Vector3.Distance(transform.position, destPosition) <= delta && dir == lastMoveDir) {
+                // destination is close enough, update destination
+                // in order to make it more smooth for long time key press
+                destPosition += GetMovementVector(dir);
+            }
+
+            return;
+        }
+
+        if (dir != DIRECTION_BUTTON.NONE) {
+            if (--changeDirCoolDown > 0)
+                return;
+            changeDirCoolDown = 0;
+
+            var movementVector = GetMovementVector(dir);
+            if (dir != lastMoveDir) {
+                // face to that dir first
+                lastMoveDir = dir;
+                FaceToDir(movementVector);
+
+                // skip 8 frames before player can move
+                changeDirCoolDown = inputDelay;
+            } else {
+                destPosition = transform.position + movementVector;
+                StartMovingAnimation(movementVector);
+            }
+        }
+    }
+
+    private bool Interact(DIRECTION_BUTTON dir, ACTION_BUTTON action) {
+        if (action == ACTION_BUTTON.NONE)
+            return false;
+
+        Vector3 dirVector = GetMovementVector(dir); // todo: change function name
+        if (dirVector == Vector3.zero)
+            return false;
+
+        RaycastHit2D hit = CheckRaycast(dirVector);
+        // Debug.DrawRay(transform.position, dirVector, Color.green);
+        if (hit.collider) {
+            // Debug.DrawRay(transform.position, dirVector, Color.red);
+            // Debug.DrawRay(transform.position, hit.point, Color.blue);
+
+            if (hit.collider.gameObject.HasComponent<Interactable>()) {
+                // Debug.Log("[raycast hit] @interactable " + hit.collider.gameObject.name);
+                hit.collider.gameObject.GetComponent<Interactable>().Interact(dir, action);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void StartMovingAnimation(Vector3 movement) {
+        mIsMoving = true;
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetFloat("LastMoveX", movement.x);
+        animator.SetFloat("LastMoveY", movement.y);
+        animator.SetBool("Moving", mIsMoving);
+    }
+
+    private void FaceToDir(Vector3 movement) {
+        // mIsMoving = false;
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetFloat("LastMoveX", movement.x);
+        animator.SetFloat("LastMoveY", movement.y);
+        animator.SetBool("Moving", false);
+    }
+
+    private void StopMoving2() {
+        mIsMoving = false;
+        animator.SetBool("Moving", mIsMoving);
+    }
+
     private void UpdateAnimation()
     {
         animator.SetFloat("MoveX", movement.PositionDiff.x);
@@ -210,11 +318,13 @@ public class MovementController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
+
         // Debug.Log("Collision ENTER between " + this.name + " and " + col.gameObject.name);
 
         lastCollidedObject = col.gameObject;
         lastCollisionDir = movement.LastDirection;
 
+        StopMoving2();
         ClampCurrentPosition();
 
         PlayCollisionSound(lastCollidedObject);
@@ -224,15 +334,15 @@ public class MovementController : MonoBehaviour
     {
         // Debug.Log("Collision STAY between " + this.name + " and " + col.gameObject.name);
 
-        lastCollidedObject = col.gameObject;
-        lastCollisionDir = movement.LastDirection;
-
-        if (movement.IsMoving)
-        {
-            PlayCollisionSound(lastCollidedObject);
-        }
-
-        StopMoving();
+//         lastCollidedObject = col.gameObject;
+//         lastCollisionDir = movement.LastDirection;
+// 
+//         if (movement.IsMoving)
+//         {
+//             PlayCollisionSound(lastCollidedObject);
+//         }
+// 
+//         StopMoving();
     }
 
     bool HasCollisionSound(GameObject gobj)
@@ -271,6 +381,10 @@ public class MovementController : MonoBehaviour
         movement.Stop();
         UpdateAnimation();
         ClampCurrentPosition();
+    }
+
+    public bool IsMoving2() {
+        return mIsMoving;
     }
 
     public bool IsMoving()
